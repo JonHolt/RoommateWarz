5ec8b47aae1e096ff6768ceaa4e36046aa0a6480
Mike C <MCluck90@gmail.com>
2016-01-17 20:48:05 -0700

    Added spawn points and purely random choice of spawn point

--- RoommateWarz/Assets/Scripts/Manager.cs
+++ RoommateWarz/Assets/Scripts/Manager.cs
@@ -17,13 +17,15 @@ public class Manager : MonoBehaviour {
                 player1 = Instantiate(player);
                 player1.GetComponent<Control>().playerNum = 1;
                 player1.GetComponent<Animator>().runtimeAnimatorController = animation1;
+				player1.transform.position = GetSpawnPoint(player1.transform.position);
                 cam.following[0] = player1.transform;
             }
             else if (!player1.activeSelf) {
 				// Respawn player 1
                 player1.SetActive(true);
                 player1.GetComponent<Control>().ResetHealth();
-                cam.following[0] = player1.transform;
+				player1.transform.position = GetSpawnPoint(player1.transform.position);
+				cam.following[0] = player1.transform;
             }
         }
         if (Input.GetButton("Start2")) {
@@ -32,13 +34,15 @@ public class Manager : MonoBehaviour {
 				player2 = Instantiate(player);
                 player2.GetComponent<Control>().playerNum = 2;
                 player2.GetComponent<Animator>().runtimeAnimatorController = animation2;
-                cam.following[1] = player2.transform;
+				player2.transform.position = GetSpawnPoint(player2.transform.position);
+				cam.following[1] = player2.transform;
             }
             else if (!player2.activeSelf) {
 				// Respawn player 2
 				player2.SetActive(true);
                 player2.GetComponent<Control>().ResetHealth();
-                cam.following[1] = player2.transform;
+				player2.transform.position = GetSpawnPoint(player2.transform.position);
+				cam.following[1] = player2.transform;
             }
         }
         #endregion
@@ -51,4 +55,23 @@ public class Manager : MonoBehaviour {
         }
         #endregion
     }
+
+	/// <summary>
+	/// Find a respawn point for the given player
+	/// </summary>
+	/// <param name="playerPosition"></param>
+	/// <returns></returns>
+	Vector3 GetSpawnPoint(Vector3 playerPosition) {
+		Vector3 spawnPoint = Vector3.zero;
+		var spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+		int index = (int)(Random.value * spawnPoints.Length);
+		while (index == spawnPoints.Length) {
+			index = (int)(Random.value * spawnPoints.Length);
+		}
+		spawnPoint = spawnPoints[index].transform.position;
+
+		// Ensure the player appears at the correct z position
+		spawnPoint.z = playerPosition.z;
+		return spawnPoint;
+	}
 }
---
+++ RoommateWarz/Assets/Scripts/RespawnPoint.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnPoint : MonoBehaviour {
+	void Start() {
+		// Remove the sprite
+		Destroy(GetComponent(typeof(SpriteRenderer)));
+	}
+}
